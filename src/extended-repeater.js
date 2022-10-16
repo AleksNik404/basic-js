ee4615548e168cfc9236502e311480ce97ec9d93
AleksNik404 <AleksNik404@gmail.com>
2022-10-16 03:21:17 +0300

    feat: solve extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,30 +16,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function repeater(str, options) {
-  throw new NotImplementedError('Not implemented');
-  // let {
-  //   repeatTimes = 1,
-  //   separator,
-  //   addition,
-  //   additionRepeatTimes,
-  //   additionSeparator,
-  // } = options;
-  // if (typeof str !== 'string') str = str + '';
-  // if (typeof addition !== 'string') addition = addition + '';
-  // let templateArr = [];
-  // for (let i = 0; i < repeatTimes; i++) {}
-  // return templateArr.join('');
-}
+  let {
+    repeatTimes = 1,
+    separator = '+',
+    addition,
+    additionRepeatTimes = 1,
+    additionSeparator = '|',
+  } = options;
 
-// console.log(
-//   repeater('STRING', {
-//     repeatTimes: 3,
-//     separator: '**',
-//     addition: 'PLUS',
-//     additionRepeatTimes: 3,
-//     additionSeparator: '00',
-//   })
-// );
+  if (typeof str !== 'undefined') str = str + '';
+  if (typeof addition !== 'undefined') addition = addition + '';
+
+  let main = [];
+  let second = [];
+
+  // create main str witch separator
+  for (let i = 0; i < repeatTimes; i++) {
+    main.push(str);
+  }
+
+  // create addition str witch additionSeparator
+  if (addition) {
+    for (let i = 0; i < additionRepeatTimes; i++) {
+      second.push(addition);
+    }
+  }
+
+  // merger addition str to main if exists
+  if (second.length !== 0) {
+    main.forEach((el, index, arr) => {
+      arr[index] = el + second.join(additionSeparator);
+    });
+  }
+
+  return main.join(separator);
+}
 
 module.exports = {
   repeater,
